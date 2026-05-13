91e0a26f956aa3756df6fd89f49f7987cc20e061
Diyar1221 <kamaletdinovdiyar@gmail.com>
2026-05-13 14:30:00 +0500

    Добавить конфигурацию для деплоя на Render

--- app/database.py
+++ app/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "mysql+pymysql://root:@localhost:3306/medical_fastapi"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost:3306/medical_fastapi")
 
-engine = create_engine(DATABASE_URL)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 class Base(DeclarativeBase):
